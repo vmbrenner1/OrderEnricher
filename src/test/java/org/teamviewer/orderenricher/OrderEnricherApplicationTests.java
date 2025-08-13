14e52ab8ef218e660c79888a2a1b1dbd52d0d9fd
Victoria Brenner <vmbrenner1@gmail.com>
2025-08-12 19:17:45 -0500

    Initial Commit to get the base Spring Boot application committed to a GitHub repository.

---
+++ src/test/java/org/teamviewer/orderenricher/OrderEnricherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teamviewer.orderenricher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEnricherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
