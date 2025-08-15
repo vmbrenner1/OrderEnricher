2b13bea08593f920cdb545ebd1298dcf599ae818
Victoria Brenner <vmbrenner1@gmail.com>
2025-08-15 04:33:36 -0500

    Commit Checkpoint. Remembered the standards of converting between DTOs and Entities and had to restructure things to adhere to this standard. EnrichedOrders are successfully retrieved and stored. If the order is not found in Redis, but found in the database, the code will save it to Redis for future attempts at retrieval.

--- src/main/java/org/teamviewer/orderenricher/model/EnrichedOrderEntity.java
+++ src/main/java/org/teamviewer/orderenricher/model/EnrichedOrderEntity.java
@@ -3,8 +3,6 @@ package org.teamviewer.orderenricher.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -29,10 +27,6 @@ import java.util.List;
 @Table(name = "enriched_order")
 public class EnrichedOrderEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "enriched_order_id")
-    private Long enrichedOrderId;
-
     @Column(name = "order_id")
     private String orderId;
 
@@ -46,7 +40,7 @@ public class EnrichedOrderEntity {
     @ManyToMany
     @JoinTable(
             name = "enriched_order_products",
-            joinColumns = @JoinColumn(name = "enriched_order_id"),
+            joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "product_id")
     )
     private List<ProductEntity> products;
