dfaa4442dea15f3d8d117b33d1db2ec210759760
Victoria Brenner <vmbrenner1@gmail.com>
2025-08-15 06:16:44 -0500

    Commit Checkpoint. Adding a few more mock entries to the database and adding a few cURL samples.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,13 @@
-INSERT INTO product (product_id, category, name, price, tags) VALUES ('1', 'Video Games', 'final fantasy xiv', 60.00, 'mmo,expansions,pvp,pve');
+INSERT INTO product (product_id, category, name, price, tags) VALUES ('1', 'Video Games', 'final fantasy xiv', 60.00, 'mmo,expansions,pvp,pve,plot-driven');
+INSERT INTO product (product_id, category, name, price, tags) VALUES ('2', 'Video Games', 'league of legends', 0.00, 'moba,pvp,champions,esports,bots');
+INSERT INTO product (product_id, category, name, price, tags) VALUES ('3', 'Video Games', 'expedition 33', 50.00, 'turn-based,rpg,french,wee-woo,plot-driven');
+INSERT INTO product (product_id, category, name, price, tags) VALUES ('4', 'Books', 'fourth wing', 20.13, 'fiction,series,rebecca yarros,dragons,fantasy');
+INSERT INTO product (product_id, category, name, price, tags) VALUES ('5', 'Books', 'city of bones', 10.82, 'fiction,series,cassandra clare,supernatural');
+INSERT INTO product (product_id, category, name, price, tags) VALUES ('6', 'Movies', 'k-pop demon hunters', 15.00, 'kpop,demon hunters,demons,love,friendship,power');
 
-INSERT INTO customer (customer_id, country, name, street, zip) VALUES ('1', 'United States', 'Victoria Brenner', 'Test Drive Rd', '75546')
+INSERT INTO customer (customer_id, country, name, street, zip) VALUES ('1', 'United States', 'Victoria Brenner', 'Test Drive Rd', '75546');
+INSERT INTO customer (customer_id, country, name, street, zip) VALUES ('2', 'Eorzea', 'Jeopardy Groveshadow', 'Haurchefant Rd', '11133');
+INSERT INTO customer (customer_id, country, name, street, zip) VALUES ('3', 'United States', 'John Turner', 'Faraway Blvd', '75943');
+INSERT INTO customer (customer_id, country, name, street, zip) VALUES ('4', 'United States', 'Freddy Blumhouse', 'Fazbear Avenue', '19875');
+INSERT INTO customer (customer_id, country, name, street, zip) VALUES ('5', 'Canada', 'Easton West', 'East Grove Trail', '36589');
+INSERT INTO customer (customer_id, country, name, street, zip) VALUES ('6', 'United States', 'Ysayle Snowpetal', 'Sunnyside Ln', '87779');
